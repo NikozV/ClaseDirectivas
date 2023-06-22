e62938da3b8c026769c454fd09f626c055a76acd
Nico <vassallo.mn@gmail.com>
2023-06-22 20:19:02 -0300

    creacion y recorrido de usuarios en table

--- src/app/componente/dashboard/dashboard.component.ts
+++ src/app/componente/dashboard/dashboard.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  titulo: string;
+
+  public usuarios= [
+    {nombre: 'Juan', apellido: 'Perez'},
+    {nombre: 'Maria', apellido: 'Gomez'},
+    {nombre: 'Pedro', apellido: 'Rodriguez'},
+    {nombre: 'Luis', apellido: 'Gonzalez'},
+    {nombre: 'Ana', apellido: 'Sanchez'},
+    {nombre: 'Jose', apellido: 'Lopez'},
+    {nombre: '', apellido: 'Garcia'},
+    {nombre: 'Carlos', apellido: ''},
+  ];
+
+  constructor() {
+    this.titulo = 'DIRECTIVAS ESTRUCTURALES';
+  }
 }
